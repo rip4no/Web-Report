c7757f32a1f60363de48e9a1050bb19bd51b2e10
rip4no <ripanogusman@gmail.com>
2025-07-16 11:56:44 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,27 @@
-import { auth } from './firebase-config.js';
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// login.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-window.login = function () {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+const firebaseConfig = {
+  apiKey: "AIzaSyBq5U_hQ8FPYEpBzYxMMO_8uXLo4lS0CZA",
+  authDomain: "karyawan-835af.firebaseapp.com",
+  projectId: "karyawan-835af",
+  storageBucket: "karyawan-835af.appspot.com",
+  messagingSenderId: "712164404160",
+  appId: "1:712164404160:web:d298319fabc87987088561"
+};
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Login berhasil, arahkan ke halaman utama
-      window.location.href = "halaman-utama.html";
-    })
-    .catch((error) => {
-      alert("Login gagal: " + error.message);
-    });
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "halaman-utama.html";
+  } catch (error) {
+    alert("Login gagal: " + error.message);
+  }
 };
