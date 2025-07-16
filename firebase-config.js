7ff2cdcbaf8461b4e58d4e7e24cf047e9b225497
rip4no <ripanogusman@gmail.com>
2025-07-16 14:14:45 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Konfigurasi Firebase kamu (dari SDK)
 const firebaseConfig = {
   apiKey: "AIzaSyBq5U_hQ8FPYEpBzYxMMO_8uXLo4lS0CZA",
   authDomain: "karyawan-835af.firebaseapp.com",
@@ -16,6 +14,12 @@ const firebaseConfig = {
   measurementId: "G-5H7FW81VBM"
 };
 
-// Initialize Firebase
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Inisialisasi Auth dan Firestore
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export supaya bisa dipakai file lain
+export { auth, db };
