60cbc9e3101b4b6ea3240db2065d487b2f1b1c40
rip4no <ripanogusman@gmail.com>
2025-07-16 12:05:43 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,8 @@
 // login.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./firebase-config.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBq5U_hQ8FPYEpBzYxMMO_8uXLo4lS0CZA",
-  authDomain: "karyawan-835af.firebaseapp.com",
-  projectId: "karyawan-835af",
-  storageBucket: "karyawan-835af.appspot.com",
-  messagingSenderId: "712164404160",
-  appId: "1:712164404160:web:d298319fabc87987088561"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
+// Penting: fungsi login harus bisa dipanggil dari index.html
 window.login = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
