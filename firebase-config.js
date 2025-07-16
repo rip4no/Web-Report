daea716edad6af6cd0eb9a39e5660523401ce741
rip4no <ripanogusman@gmail.com>
2025-07-16 14:08:43 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,25 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Konfigurasi Firebase kamu (dari SDK)
 const firebaseConfig = {
   apiKey: "AIzaSyBq5U_hQ8FPYEpBzYxMMO_8uXLo4lS0CZA",
   authDomain: "karyawan-835af.firebaseapp.com",
   projectId: "karyawan-835af",
-  storageBucket: "karyawan-835af.appspot.com", // ✅ sudah diperbaiki
+  storageBucket: "karyawan-835af.firebasestorage.app",
   messagingSenderId: "712164401460",
   appId: "1:712164401460:web:d293819fabc87907008561",
   measurementId: "G-5H7FW81VBM"
 };
 
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+
+// Inisialisasi Auth dan Firestore
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export supaya bisa dipakai file lain
+export { auth, db };
